7d4145e3771e3a94d0c031e39b21bf24902a5b01
MikeChara <mikejchara@gmail.com>
2022-11-08 12:31:47 +0000

    task 2 done

--- task2.js
+++ task2.js
@@ -1,11 +1,14 @@
 let animals = [
-    "baboon",
-    "kangaroo",
-    "rhino",
-    "frog",
-    "beaver",
-    "horse",
-    "basalisk",
-  ];
-  
-let bListAnimals = // Your 'filter' code here.
+  "baboon",
+  "kangaroo",
+  "rhino",
+  "frog",
+  "beaver",
+  "horse",
+  "basalisk",
+];
+
+let bListAnimals = animals.filter((pet) => {
+  return pet.charAt(0) == "b";
+});
+console.log(bListAnimals);
