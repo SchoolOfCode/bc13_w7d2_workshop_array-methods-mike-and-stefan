a42cfe54c3649e4568bfcb4bf618854c132df00b
MikeChara <mikejchara@gmail.com>
2022-11-08 14:06:36 +0000

    halfway task 3

--- task3.js
+++ task3.js
@@ -1,1 +1,11 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+let three = 3;
+let five = 5;
+let sixty = 60;
+let ninety = 90;
+const multiplesThree = someNumbers.some((number) => number / three !== 0);
+console.log(multiplesThree);
+// constnumberiplesnumber/three == IntegerFive = someNumbers.some((someNumbers) => );
+// const multiplesSixty = someNumbers.some((someNumbers) =>s;
+// const multiplesNinety = someNumbers.some((someNumbers) => )
